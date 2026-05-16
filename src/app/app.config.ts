f8beffdcdf20ea5fd9e488237252da3e03c16aad
sumou <zalicsu@gmail.com>
2026-05-17 04:03:22 +0800

    feat: 更新樣式及配置，整合Angular Material主題與字型

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideAnimations(),
     provideRouter(routes)
   ]
 };
