dc9ce1a8c1643350d89016811bd3c5a7243b0ec3
smm_2271 <zalicleexu@gmail.com>
2026-05-17 16:12:04 +0000

    feat: 新增路由設定以支援 Home、Devs、About 和 Contact 頁面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Devs } from './pages/devs/devs';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'home',
+        loadComponent:() => import('./pages/home/home').then(m => m.Home),
+    },
+    {
+        path: 'devs',
+        loadComponent:() => import('./pages/devs/devs').then(m => m.Devs),
+    },
+    {
+        path: 'about',
+        loadComponent:() => import('./pages/about/about').then(m => m.About),
+    },
+    {
+        path: 'contact',
+        loadComponent:() => import('./pages/contact/contact').then(m => m.Contact),
+    }
+];
