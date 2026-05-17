57adab1881c37eec65eec4c656fe68ccf45eac5e
smm_2271 <zalicleexu@gmail.com>
2026-05-17 16:01:35 +0000

    feat: 新增 About、Devs 和 Home 頁面

---
+++ src/app/pages/devs/devs.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devs',
+  imports: [],
+  templateUrl: './devs.html',
+  styleUrl: './devs.scss',
+})
+export class Devs {}
