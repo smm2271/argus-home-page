2c5d8c49ff387c5d98119b0549434ffc2f71bfcc
smm_2271 <zalicleexu@gmail.com>
2026-05-22 18:22:14 +0000

    refactor: 抽出footer組件及樣式，並更新主頁面以包含頁腳

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './share/nav/nav';
+import { Footer } from './share/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, RouterOutlet],
+  imports: [Nav, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/share/footer/footer.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.html',
+  styleUrls: ['./footer.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer {}
